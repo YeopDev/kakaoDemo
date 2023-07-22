03837438b3afdd02138d3a3c8a43ca58d83be9ea
sangyeop <yeop1.dev@gmail.com>
2023-07-22 22:02:00 +0900

    :recycle: refactor(RoomRepository): 컴포넌트 분리

--- kakao/src/main/java/com/demo/kakao/repository/room/RoomRepository.java
+++ kakao/src/main/java/com/demo/kakao/repository/room/RoomRepository.java
@@ -1,5 +1,6 @@
-package com.demo.kakao.domain;
+package com.demo.kakao.repository.room;
 
+import com.demo.kakao.domain.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
